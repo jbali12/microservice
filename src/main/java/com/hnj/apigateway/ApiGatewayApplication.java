c4dc4771236dce57cd9478428e205ef6e3ca0726
jbali12 <88539149+jbali12@users.noreply.github.com>
2022-05-22 12:38:05 +0200

    Update ApiGatewayApplication.java

--- src/main/java/com/hnj/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/hnj/apigateway/ApiGatewayApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+
 
 @EnableDiscoveryClient
 @EnableZuulProxy
