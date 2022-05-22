ebdf7538f2c12046d85f56795aab6ed0abc92b13
jbali12 <88539149+jbali12@users.noreply.github.com>
2022-05-22 12:26:29 +0200

    Update ApiGatewayApplication.java

--- src/main/java/com/hnj/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/hnj/apigateway/ApiGatewayApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableDiscoveryClient
 @EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class ApiGatewayApplication {
 	public static void main(String[] args) {
