46e28477eb21f37ec6d4f7fd6a7f0a8ed325762f
jbali12 <88539149+jbali12@users.noreply.github.com>
2022-05-30 17:11:43 +0200

    Update OfferController.java

--- src/main/java/com/hnj/offer/controller/OfferController.java
+++ src/main/java/com/hnj/offer/controller/OfferController.java
@@ -10,7 +10,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "*")
 public class OfferController {
 	private OfferService offerService;
 
