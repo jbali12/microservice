b6a8608f772c734dd212f22db2c700bdb9165b4c
jbali12 <88539149+jbali12@users.noreply.github.com>
2022-05-30 17:12:22 +0200

    Update ProductController.java

--- src/main/java/com/hnj/product/controller/ProductController.java
+++ src/main/java/com/hnj/product/controller/ProductController.java
@@ -10,7 +10,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "*")
 public class ProductController {
 	private ProductService productService;
 
