76c312b6ea758aa200917f224efcd90f4cdda4e0
jbali12 <88539149+jbali12@users.noreply.github.com>
2022-05-30 15:31:41 +0200

    Update ProductController.java

--- src/main/java/com/hnj/product/controller/ProductController.java
+++ src/main/java/com/hnj/product/controller/ProductController.java
@@ -10,7 +10,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins="http://microservice:3000", allowedHeaders="*")
+@CrossOrigin
 public class ProductController {
 	private ProductService productService;
 
